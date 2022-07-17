ff4a60bf8f7aeeef0376b6b8912e134286558645
roxy22r <raksana280@gmail.com>
2022-07-17 20:10:47 +0200

    R22-> Set Pawn all Pawns on Board

--- Play.py
+++ Play.py
@@ -5,6 +5,7 @@ import Piece
 
 
 class Play:
+
     def __init__(self, board: Board):
         self.board = board
 
@@ -16,4 +17,4 @@ class Play:
             print("Please Enter Col Number")
             col = input()
             piece: Piece = self.board[row][col]
-            self.board[row][col]: Piece = None
+            self.board[row][col]: Piece.Piece = None
